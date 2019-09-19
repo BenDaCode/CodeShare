bd9569111bd993a74c2f199783ca0e896be65ea2
Benedikt Duerrschmid <u697723@footlocker.com>
2019-09-19 15:47:52 +0200

    adding css

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var content="";
+var rooms={};
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  
-  io.emit('codecontent', content);
+app.get('/rooms/:room', function(req, res){
+  var room = req.params.room; 
+  res.render('room', {
+    room: room
+  })
+});
 
-  socket.on('codecontent', function(code){
-      content=code;
-      io.emit('codecontent', code);
-      console.log(content);
+io.sockets.on('connection', function(socket) {
+
+  socket.on('room', function(room) {
+      socket.join(room);
+    
+      if(typeof rooms[room] !== "undefined"){
+        var content= rooms[room].content;
+        io.sockets.in(room).emit('updateCode', content);
+      }else{
+        rooms[room]={};
+      }
+      socket.on('updateCode', function(code){
+        rooms[room].content=code;
+        io.sockets.in(room).emit('updateCode', code);
+      });
   });
+
+  io.sockets.emit('getRooms', rooms);
+
 });
 
 http.listen(3000, function(){
